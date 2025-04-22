ace96163701c13852153ceb8ec374159409a2527
rajv4rdhan <99574182+rajv4rdhan@users.noreply.github.com>
2025-04-22 20:16:18 +0530

    feat: Update S3 configuration with new bucket name and credentials

--- builder/src/config.js
+++ builder/src/config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    BUCKET_NAME: 'static322423',
-    ACCESS_KEY_ID: 'AKIA6NYQMT3BPDZUQQBF',
-    SECRET_ACCESS_KEY: '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',
+    BUCKET_NAME: 'ewer324324',
+    ACCESS_KEY_ID: 'AKIA6NYQMT3BIHGWGMCG',
+    SECRET_ACCESS_KEY: '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',
     S3_01: 'sdfesd',
     S3_FOLDER: 'sdfesd/build',
     LOCAL_DIR: path.join(__dirname, 'local_build'),
