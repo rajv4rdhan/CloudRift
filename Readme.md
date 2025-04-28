da8593238ed254ae71ef5cf87a99e20c7892ffa2
rajv4rdhan <99574182+rajv4rdhan@users.noreply.github.com>
2025-04-28 10:23:00 +0530

    feat: Update configuration for S3 and Nginx with new bucket and backend URLs

--- Readme.md
+++ Readme.md
@@ -6,4 +6,11 @@ Basic
 
 Advance
 - [ ] Add support for react app (build worker)
-- [ ] 
+
+
+
+
+
+AWS Secrets Manager
+AWS Shield
+AWS WAF
--- builder/src/config.js
+++ builder/src/config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    BUCKET_NAME: 'static322423',
-    ACCESS_KEY_ID: 'AKIA6NYQMT3BPDZUQQBF',
-    SECRET_ACCESS_KEY: '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',
+    BUCKET_NAME: 'cloudrift43',
+    ACCESS_KEY_ID: 'AKIA6NYQMT3BMQC45XUC',
+    SECRET_ACCESS_KEY: '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',
     S3_01: 'sdfesd',
     S3_FOLDER: 'sdfesd/build',
     LOCAL_DIR: path.join(__dirname, 'local_build'),
