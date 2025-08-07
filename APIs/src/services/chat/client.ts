df5b81cdfb2e82da171797de85d5bed1c1a4f4d4
rajv4rdhan <99574182+rajv4rdhan@users.noreply.github.com>
2025-08-07 17:17:04 +0530

    fixed editor

--- APIs/src/services/chat/client.ts
+++ APIs/src/services/chat/client.ts
@@ -19,7 +19,7 @@ type MessageRequest = { message: string };
 class ChatClient {
   private client: any;
 
-  constructor(host: string = 'rnpaf-34-81-230-112.a.free.pinggy.link:38639') {
+  constructor(host: string = 'rnsvd-34-16-199-107.a.free.pinggy.link:34513') {
     this.client = new chatProto.chat.ChatService(
       host,
       grpc.credentials.createInsecure()
